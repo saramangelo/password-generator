2f08666d5d3d4bc7fdd6531df9327b47c11dfd87
saramangelo <saramangelo@gmail.com>
2022-11-28 23:46:51 -0800

    added code to validate user input

--- 02-Challenge/Develop/script.js
+++ 02-Challenge/Develop/script.js
@@ -1,3 +1,31 @@
+var upperCase = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
 var lowerCase = [
   "a",
   "b",
@@ -27,38 +55,9 @@ var lowerCase = [
   "z",
 ];
 
-var upperCase = [
-  "A",
-  "B",
-  "C",
-  "D",
-  "E",
-  "F",
-  "G",
-  "H",
-  "I",
-  "J",
-  "K",
-  "L",
-  "M",
-  "N",
-  "O",
-  "P",
-  "Q",
-  "R",
-  "S",
-  "T",
-  "U",
-  "V",
-  "W",
-  "X",
-  "Y",
-  "Z",
-];
-
 var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
-var symbols = [
+var specialChar = [
   "!",
   '"',
   "#",
@@ -93,7 +92,10 @@ var symbols = [
   "~",
 ];
 
+
 var passwordArray = [];
+
+
 
 // from office hours notes:
 // var possiblePassword = [];
@@ -107,6 +109,7 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function generatePassword() {
+  
   // password has to be between 8 and 128 characters, needs prompting if erroneous input
 
   var characterNumberMessage =
@@ -123,7 +126,8 @@ function generatePassword() {
     userInput = prompt(characterNumberMessage);
   }
 
-  // user is presented with question - do you want to include 1. uppercase? 2. lowercase? 3. special character? 4. number?
+  console.log(userInput);
+  // user is presented with question - do you want to include 1. uppercase? 2. lowercase? 3. number? 4. special character?
   // user selects which criteria to include
 
   var upperCaseChoice = confirm(
@@ -134,13 +138,16 @@ function generatePassword() {
     "Do you want to include a lowercase letter in your password?"
   );
 
-  var specialCharChoice = confirm(
-    "Do you want to include a special character in your password?"
-  );
-
   var numberChoice = confirm(
     "Do you want to include a number in your password?"
   );
+  var specialCharChoice = confirm(
+    "Do you want to include a special character in your password?"
+  );
+  console.log(upperCaseChoice);
+  console.log(lowerCaseChoice);
+  console.log(numberChoice);
+  console.log(specialCharChoice);
 
   if (
     !upperCaseChoice &&
@@ -167,30 +174,49 @@ function generatePassword() {
   } else {
     // move to next step
   }
-  return "this needs to be all variables concatenated"; //  change
-}
+  return "this needs to be all variables randomized and concatenated"; //  change
 
 // need to utilize variable arrays above / validate input
 
-// if (upperCaseChoice){
-//   passwordArray
-//   // take all upper case letters, add to final array
-// }
+if (upperCaseChoice){
+  passwordArray += upperCase;
+  // take all upper case letters, add to final array
+}
 
-// if (lowerCaseChoice){
-//   // take all lower case letters, add to final array
-// }
+if (lowerCaseChoice){
+   passwordArray += lowerCase;
+  // take all lower case letters, add to final array
+}
 
-// if (specialCharChoice){
-//   // take all special characters, add to final array
-// }
+if (specialCharChoice){
+   passwordArray += specialChar;
+  // take all special characters, add to final array
+}
 
-// if (numberChoice){
-//   // take all numbers, add to final array
-// }
+if (numberChoice){
+   passwordArray += numbers;
+  // take all numbers, add to final array
+}
 
+for(i = 0; i < upperCase.length; i++){
+  upperCase[Math.floor(Math.random()*upperCase.length)];
+  console.log(upperCaseChoice);
+}
+for(i = 0; i < lowerCase.length; i++){
+  lowerCase[Math.floor(Math.random()*lowerCase.length)];
+  console.log(lowerCase);
+}
+for(i = 0; i < specialChar.length; i++){
+  specialChar[Math.floor(Math.random()*specialChar.length)];
+  console.log(specialChar);
+}
+for(i = 0; i < numbers.length; i++){
+  numbers[Math.floor(Math.random()*numbers.length)];
+  
+  console.log(numbers);
+}
 
-
+}
 
 
 function writePassword() {
